fba673683eadbcc8004f2837d49d64da47cbf413
Aditya Bayu <97433258+iniadittt@users.noreply.github.com>
2024-01-09 19:14:08 +0700

    fix bug: favorite

--- src/favorite/favorite-route.js
+++ src/favorite/favorite-route.js
@@ -7,6 +7,6 @@ const FavoriteRouting = express.Router()
 
 FavoriteRouting.get('/favorites/me', Authentication, FavoriteController.getMyFavorites, errorMiddleware)
 FavoriteRouting.post('/favorite/:id', Authentication, FavoriteController.postFavorite, errorMiddleware)
-FavoriteRouting.delete('/favorite/id', Authentication, FavoriteController.deleteFavorite, errorMiddleware)
+FavoriteRouting.delete('/favorite/:id', Authentication, FavoriteController.deleteFavorite, errorMiddleware)
 
 module.exports = FavoriteRouting
